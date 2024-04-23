5362a8190d81c860800d3b2961abca5b5cdb87c2
N1ph1l1m <zerorad@inbox.ru>
2024-04-23 14:26:42 +0300

    Test models.py . Create tables database

--- app/blogengine/blog/models.py
+++ app/blogengine/blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Worker(models.Model):
+     name = models.CharField(max_length=150,blank=False)
+     second_name = models.CharField(max_length=150,blank=False)
+     salary = models.IntegerField(default=0)
+
+
+
+class Color_test(models.Model):
+     name = models.CharField(max_length=150,blank=False)
+     color_typy = models.IntegerField(default=100)
+
+
+class Color_test2(models.Model):
+     name = models.CharField(max_length=150,blank=False)
+     color_typy = models.IntegerField(default=100)
+     color_typys = models.IntegerField(default=100)
