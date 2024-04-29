2634b31647746a5144050e6bcae1fbab8a46bb7b
N1ph1l1m <87586541+N1ph1l1m@users.noreply.github.com>
2024-04-29 23:18:22 +0300

    Version 1.5  - Class based views . Use mixins

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Post , Tag
+from django.views.generic import View
+from .utils import ObjectDetailMixin
 
 def posts_list(request):
     posts = Post.objects.all
@@ -8,20 +11,27 @@ def posts_list(request):
 
 
 
-
-def post_detail(request,slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context = {'post': post})
-
-
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context = {'tags':tags})
 
+class PostDetail(ObjectDetailMixin,View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+    # def get(self,request,slug):
+    #         post = get_object_or_404(Post,slug__iexact=slug)
+    #         return render(request, 'blog/post_detail.html', context = {'post': post})
+    
+class Tag_detail(ObjectDetailMixin,View):
+     
+    model = Tag
+    template =  'blog/tag_detail.html'
+    #  def get(self,request,slug):
+    #         tag = get_object_or_404(Tag, slug__iexact=slug)
+    #         return render (request, 'blog/tag_detail.html', context = {'tag':tag} )
+        
 
 
-def tag_detail(request,slug):
-    tag = Tag.objects.get(slug__iexact = slug)
-    return render (request, 'blog/tag_detail.html', context = {'tag':tag} )
 
 # Create your views here.
