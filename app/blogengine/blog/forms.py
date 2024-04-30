6b2e500d8bd606ffd0659813bb96d2312d53eda6
N1ph1l1m <zerorad@inbox.ru>
2024-04-30 11:17:59 +0300

    Version 1.7.1 - Work post form part 1

--- app/blogengine/blog/forms.py
+++ app/blogengine/blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Tag
+from .models import Tag,Post
 from django.core.exceptions import ValidationError
 
 class TagForm(forms.ModelForm):
@@ -9,7 +9,7 @@ class TagForm(forms.ModelForm):
     # title.widget.attrs.update({'class':'form-control'})
     # slug.widget.attrs.update({'class':'form-control'})
 
-    class Meta: 
+    class Meta:
         model = Tag
         fields = ['title','slug']
 
@@ -31,3 +31,31 @@ class TagForm(forms.ModelForm):
     # def save(self):
     #     new_tag = Tag.objects.create(title = self.cleaned_data['title'], slug = self.cleaned_data['slug'])
     #     return new_tag
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug' , 'bodu' , 'tags',]
+
+        widget = {
+            'title':forms.TextInput(attrs={'class':'form-control'}),
+            'slug':forms.TextInput(attrs={'class':'form-control'}),
+            'bodu':forms.Textarea(attrs={'class':'form-control'}),
+            'tags':forms.SelectMultiple(attrs={'class':'form-control'}),
+        }
+
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+
+        if new_slug == 'create':
+            raise ValidationError('Slug mat not be "Create "')
+        return new_slug
+
+
+        #  title = models.CharField(max_length = 150, db_index = True)
+        # slug = models.SlugField(max_length = 150, unique = True)
+        # bodu = models.TextField(blank = True, db_index = True)
+        # date_pub = models.DateTimeField(auto_now_add = True)
+        # tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -5,6 +5,7 @@ from .models import Post , Tag
 from django.views.generic import View
 from .utils import ObjectDetailMixin
 from .forms import TagForm
+from .forms import PostForm
 
 
 def posts_list(request):
@@ -25,15 +26,15 @@ class PostDetail(ObjectDetailMixin,View):
     # def get(self,request,slug):
     #         post = get_object_or_404(Post,slug__iexact=slug)
     #         return render(request, 'blog/post_detail.html', context = {'post': post})
-    
+
 class Tag_detail(ObjectDetailMixin,View):
-     
+
     model = Tag
     template =  'blog/tag_detail.html'
     #  def get(self,request,slug):
     #         tag = get_object_or_404(Tag, slug__iexact=slug)
     #         return render (request, 'blog/tag_detail.html', context = {'tag':tag} )
-        
+
 class TagCreate(View):
     def get(self,request):
         form = TagForm()
@@ -46,5 +47,18 @@ class TagCreate(View):
             new_tag = bound_form.save()
             return redirect(new_tag)
         return render(request, 'blog/tag_create.html', context={'form':bound_form})
+
+class PostCreate(View):
+    def get(self,request):
+        form = PostForm()
+        return render(request, "blog/post_create.html", context = {'form':form})
+
+    def post(self,request):
+        bound_form = PostForm(request.POST)
+
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, "blog/post_create.html", context = {'form':bound_form})
 
 # Create your views here.
