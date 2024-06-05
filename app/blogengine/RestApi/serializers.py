b0673ee8452b3b4acaea2db4637406faf8942d18
N1ph1l1m <zerorad@inbox.ru>
2024-06-05 14:58:12 +0300

    2.2 - Serializers]

--- app/blogengine/RestApi/serializers.py
+++ app/blogengine/RestApi/serializers.py
@@ -1,16 +1,50 @@
+import io
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from .models import Women
 
 
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Women
-        fields = ('title' , 'cat_id')
+# ## класс словарь с данными
+# class WomenModel:
+#     def __init__(self,title,content):
+#         self.title = title
+#         self.content = content
 
 
+# ## класс сериализатор для данных.Заголовки должны совподать
+# class WomenSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=255)
+#     content = serializers.CharField()
 
-# class WomenAPIView(APIView):
-#     def get(self,request):
-#         return Response({'title':'Angelina Jolie'})
+
+# # преобразование данных в JSON формат
+# def encode():
+#     model = WomenModel('Angelina Lolie', 'Content: Angelina Jolie  - is actor')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data,type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+
+# # class WomenAPIView(APIView):
+# #     def get(self,request):
+# #         return Response({'title':'Angelina Jolie'})
+
+# ## декодирование полученных данных
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Mark Spector", "content":"Content : Is kindom moon"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data = data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only = True)
+    time_update = serializers.DateTimeField(read_only = True)
+    is_published = serializers.BooleanField(default = True)
+    cat_id = serializers.IntegerField()
--- app/blogengine/RestApi/views.py
+++ app/blogengine/RestApi/views.py
@@ -23,13 +23,18 @@ def test(request):
 
 class WomenAPIView(APIView):
     def get(self,request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})
+        w  = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many = True).data})
 
     def post(self,request):
+
+        serializer = WomenSerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+
+
         post_new = Women.objects.create(
             title = request.data['title'],
             content  = request.data['content'],
             cat_id = request.data['cat_id']
         )#добавление записи в бд
-        return Response({'post': model_to_dict(post_new)}) #возвращает результат
+        return Response({'post':WomenSerializer(post_new).data}) #возвращает результат
