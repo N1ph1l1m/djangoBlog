8e3ec67a151fc18b9db9cf649d6fc76c690d6cc7
N1ph1l1m <zerorad@inbox.ru>
2024-06-24 14:21:48 +0300

    RestFramework doc 1.2 . Serializer

--- app/blogengine/quick_start/urls.py
+++ app/blogengine/quick_start/urls.py
@@ -2,13 +2,16 @@ from django.urls import path,include
 from .views import *
 from rest_framework import routers
 
+
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups',GroupViewSet)
 
 
+
 urlpatterns = [
       path('', test, name='test'),
-      path('api-auth/', include('rest_framework.urls', namespace= 'rest_framework'))
-
+      path('api-auth/', include('rest_framework.urls', namespace= 'rest_framework')),
+      path('snippets/', snippet_list, name = 'snippet_list'),
+      path('snippets/<int:pk>/',snippet_detail, name = 'snippet_detail'),
 ]
